2d797b363a3ccb71cffaea253e3587e8661fd7a0
Ronald van Zon <34279265+DamornTyde@users.noreply.github.com>
2020-01-08 13:37:16 +0100

    The upload

---
+++ functions.js
@@ -0,0 +1,40 @@
+'use strict';
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+const steps = 30000;
+const grid = Math.PI * 2 / steps;
+
+canvas.width = 800;
+canvas.height = canvas.width;
+ctx.fillStyle = "#fff";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+var radius = canvas.width / 2;
+ctx.translate(radius, radius);
+radius = radius * 0.9;
+var frame = 0;
+
+window.requestAnimationFrame(animate);
+
+function animate() {
+    const gap = grid * frame;
+    ctx.beginPath();
+    ctx.fillStyle = "rgba(255, 255, 255, 0.01)"
+    ctx.fillRect(-canvas.width / 2, -canvas.height / 2, canvas.width, canvas.height)
+    ctx.beginPath();
+    ctx.moveTo(0, -radius);
+    for (var i = 1; i < 11; i++) {
+        ctx.rotate(gap * i);
+        ctx.lineTo(0, -radius);
+    }
+    ctx.strokeStyle = "#000";
+    ctx.lineWidth = radius * 0.015;
+    ctx.lineCap = "round";
+    ctx.lineJoin = "round";
+    ctx.stroke();
+    ctx.rotate(-gap * 55);
+    frame++;
+    if (frame == steps) {
+        frame = 0;
+    }
+    window.requestAnimationFrame(animate);
+}
