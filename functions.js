b250c0b1d9e67f3a1089e367d22b2a85d290ef2a
Ronald van Zon <34279265+DamornTyde@users.noreply.github.com>
2020-07-23 02:34:20 +0200

    Update functions.js

--- functions.js
+++ functions.js
@@ -12,6 +12,7 @@ var radius = canvas.width / 2;
 ctx.translate(radius, radius);
 radius = radius * 0.9;
 var frame = 0;
+var lines = 10;
 
 window.requestAnimationFrame(animate);
 
@@ -22,7 +23,7 @@ function animate() {
     ctx.fillRect(-canvas.width / 2, -canvas.height / 2, canvas.width, canvas.height);
     ctx.beginPath();
     ctx.moveTo(0, -radius);
-    for (var i = 1; i < 11; i++) {
+    for (var i = 1; i < lines + 1; i++) {
         ctx.rotate(gap * i);
         ctx.lineTo(0, -radius);
     }
@@ -31,10 +32,15 @@ function animate() {
     ctx.lineCap = "round";
     ctx.lineJoin = "round";
     ctx.stroke();
-    ctx.rotate(-gap * 55);
+    ctx.rotate((-gap) * lines * (lines + 1) / 2);
     frame++;
     if (frame == steps) {
         frame = 0;
     }
     window.requestAnimationFrame(animate);
 }
+
+document.getElementById("lines").addEventListener("change" ,function(this) {
+	lines = Number(this.value);
+	frame = 0;
+});
